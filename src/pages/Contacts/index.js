c255cd4f9d5c00aa90650f39e81603c8087e0f7f
Serhii Komarychev <komarichev@gmail.com>
2020-11-16 17:37:56 +0200

    fetch contacts

--- src/pages/Contacts/index.js
+++ src/pages/Contacts/index.js
@@ -1,3 +1,30 @@
+import { useState, useEffect } from "react";
+
 export function Contacts() {
-  return <div className="Contacts">Contacts</div>;
+  const [contacts, setContacts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  useEffect(() => {
+    return () => {
+      setIsLoading(true);
+      fetch("https://randomuser.me/api/?results=100")
+        .then((response) => response.json())
+        .then(({ results }) => {
+          setContacts(results);
+          setIsLoading(false);
+          setIsError(false);
+        })
+        .catch(() => {
+          setIsLoading(false);
+          isError(true);
+        });
+    };
+  }, []);
+  if (isLoading) {
+    return <div> ....Loading </div>;
+  }
+  if (isError) {
+    return <div> ...Error </div>;
+  }
+  return <div className="contacts"> {contacts[0].name.first} </div>;
 }
