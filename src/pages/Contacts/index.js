18459fc27670285a05bdf8b09150372c89581d4f
Serhii Komarychev <skomarychev@gmail.com>
2020-11-16 23:53:28 +0200

    finish add contact function

--- src/pages/Contacts/index.js
+++ src/pages/Contacts/index.js
@@ -1,30 +1,45 @@
 import { useState, useEffect } from "react";
-
-export function Contacts() {
-  const [contacts, setContacts] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+const useContacts = () => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
   useEffect(() => {
-    return () => {
-      setIsLoading(true);
-      fetch("https://randomuser.me/api/?results=100")
-        .then((response) => response.json())
-        .then(({ results }) => {
-          setContacts(results);
-          setIsLoading(false);
-          setIsError(false);
-        })
-        .catch(() => {
-          setIsLoading(false);
-          isError(true);
-        });
+    const getContacts = async () => {
+      try {
+        setIsLoading(true);
+        const response = await fetch("https://randomuser.me/api/?results=100");
+        const { results, error } = await response.json();
+        if (error) throw Error("No data");
+        console.log(results);
+        setData(results);
+        setIsError(false);
+      } catch (e) {
+        console.error(e);
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
     };
+    getContacts();
   }, []);
-  if (isLoading) {
+  return {
+    isLoading,
+    isError,
+    data,
+  };
+};
+export function Contacts() {
+  const contacts = useContacts();
+  if (contacts.isLoading) {
     return <div> ....Loading </div>;
   }
-  if (isError) {
+  if (contacts.isError) {
     return <div> ...Error </div>;
   }
-  return <div className="contacts"> {contacts[0].name.first} </div>;
+  return (
+    <div className="contacts">
+      {" "}
+      {JSON.stringify(contacts.data[0].name.first)}{" "}
+    </div>
+  );
 }
